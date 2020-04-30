322d606431f7a5803900abe66ec704cd0927229d
faculuq <56322143+faculuq@users.noreply.github.com>
2020-04-30 15:47:20 -0300

    ABM EstadosCategorias

---
+++ Proyecto Visual/uniON/uniON.AD/cEstadosCategorias.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Public Class cEstadosCategorias
+    Dim oDatabase As Database
+
+    Public Sub New()
+        oDatabase = DatabaseFactory.CreateDatabase("Conn")
+    End Sub
+
+    Public Sub New(ByVal str As String)
+    End Sub
+
+    Public Function BuscarPorId(ByVal IdEstadoCategoria As Integer) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("EstadosCategoriasBuscarPorId", IdEstadoCategoria)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function BuscarTodos() As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("EstadosCategoriasBuscarTodos")
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Agregar(ByVal Descripcion As String, ByVal Activo As Boolean) As Double
+        Try
+            Return oDatabase.ExecuteScalar("EstadosCategoriasAgregar", Descripcion, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Modificar(ByVal IdEstadoCategoria As Integer, ByVal Descripcion As String, ByVal Activo As Boolean) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("EstadosCategoriasModificar", IdEstadoCategoria, Descripcion, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+    Public Function Eliminar(ByVal IdEstadoCategoria As Integer) As Double
+        Return oDatabase.ExecuteScalar("EstadosCategoriasEliminar", IdEstadoCategoria)
+    End Function
+
+
+End Class
--- Proyecto Visual/uniON/uniON/My Project/Application.Designer.vb
+++ Proyecto Visual/uniON/uniON/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.uniON.Form1
+            Me.MainForm = Global.uniON.frmMenu
         End Sub
     End Class
 End Namespace
