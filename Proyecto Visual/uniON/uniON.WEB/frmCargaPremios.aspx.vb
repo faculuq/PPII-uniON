40729cfc9ef0e3c7734174e64f832cf1a669c67a
ArceBrian <brianalexanderarce@gmail.com>
2020-05-22 13:49:34 -0300

    Diseño frmCargaPremios

--- Proyecto Visual/uniON/uniON.WEB/frmCargaPremios.aspx.vb
+++ Proyecto Visual/uniON/uniON.WEB/frmCargaPremios.aspx.vb
@@ -1,11 +1,8 @@
-﻿Public Class frmCargaDatos
+﻿Public Class frmCargaPremios
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
-    Protected Sub TxtDescripcion_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
