39abbe12622bdd1bf076b6a9197a9c84f5f4562a
faculuq <56322143+faculuq@users.noreply.github.com>
2020-06-04 18:33:45 -0300

    comprobacion existencia de email

--- Proyecto Visual/uniON/uniON.WEB/frmRegistro.aspx.vb
+++ Proyecto Visual/uniON/uniON.WEB/frmRegistro.aspx.vb
@@ -12,38 +12,47 @@ Public Class frmRegistro
         Dim oUsuario As New cUsuarios
         Dim oPersona As New cPersonas
         Dim oEmpresa As New cEmpresas
+        Dim iduser As Integer
+        Dim existencia As Integer
 
         If txtEmail.Text <> Nothing And txtPassword.Text <> Nothing And txtPasswordConfirmar.Text <> Nothing Then
 
             If txtPassword.Text = txtPasswordConfirmar.Text Then
 
-                If checkbox.Checked = False Then
+                existencia = oUsuario.ComprobarExistencia(txtEmail.Text)
 
-                    Dim iduser As Integer
-                    Dim idpersona As Integer
-                    iduser = oUsuario.Agregar(txtEmail.Text, txtPassword.Text, 1)
-                    idpersona = oPersona.Agregar(iduser)
-                    Session("IdUsuario") = iduser
-                    Session("IdPersona") = idpersona
-                    ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaExito()", True)
-                    Response.Redirect("frmCargaDatosPersona.aspx")
+                If existencia = 1 Then
 
+                    If checkbox.Checked = False Then
+
+                        Dim idpersona As Integer
+                        iduser = oUsuario.Agregar(txtEmail.Text, txtPassword.Text, 1)
+                        idpersona = oPersona.Agregar(iduser)
+                        Session("IdUsuario") = iduser
+                        Session("IdPersona") = idpersona
+                        ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaExito()", True)
+                        Response.Redirect("frmCargaDatosPersona.aspx")
+
+                    Else
+
+                        Dim idempresa As Integer
+                        iduser = oUsuario.Agregar(txtEmail.Text, txtPassword.Text, 2)
+                        idempresa = oEmpresa.Agregar(iduser)
+                        Session("IdUsuario") = iduser
+                        Session("IdEmpresa") = idempresa
+                        ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaExito()", True)
+                        Response.Redirect("frmCargaDatosEmpresa.aspx")
+
+                    End If
                 Else
 
-                    Dim iduser As Integer
-                    Dim idempresa As Integer
-                    iduser = oUsuario.Agregar(txtEmail.Text, txtPassword.Text, 2)
-                    idempresa = oEmpresa.Agregar(iduser)
-                    Session("IdUsuario") = iduser
-                    Session("IdEmpresa") = idempresa
-                    ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaExito()", True)
-                    Response.Redirect("frmCargaDatosEmpresa.aspx")
+                    MsgBox("Este mail ya se encuentra registrado")
 
                 End If
 
             Else
 
-                ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaPass()", True)
+                    ScriptManager.RegisterClientScriptBlock(Me, GetType(String), "mensaje", "alertaPass()", True)
 
             End If
 
